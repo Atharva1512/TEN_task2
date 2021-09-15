233d87bd28660980f42b29c7dc4ec92e81f2454d
Atharva1512 <borawakeatharva2002@gmail.com>
2021-09-15 19:47:11 +0530

    swagger

--- feedback/views.py
+++ feedback/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import redirect, render, get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
 
 from .serializers import feedbackSerializer
 from .models import feedback
+from feedback import serializers
 # Create your views here.
 
 class test(APIView):
+
+    serializer_class= feedbackSerializer
     def get(self,request):
         # return render(request,"index.html")
         sgs=feedback.objects.all()
--- task2/urls.py
+++ task2/urls.py
@@ -17,8 +17,27 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from feedback import views
+# from rest_framework_swagger.views import get_swagger_view
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Docs",
+      default_version='v1',
+      description="Feedback and suggestions",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("feedback.urls"))
+    path('', include("feedback.urls")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
